7cb37fe7dfdf9f69fa34400765c24b8a31cd97d9
marti <martibarri23@gmail.com>
2017-08-19 23:48:50 +0200

    padPKCS7 bug fix

--- Set2/challenge09.py
+++ Set2/challenge09.py
@@ -1,8 +1,11 @@
 # Implement PKCS#7 padding
 
 def pad_PKCS(text, l):
-	pad = l - (len(text) % l)
-	return text + bytes([pad]*pad)
+	pad = (l - (len(text) % l)) % l
+	if len(text) != 0: 
+		return text + bytes([pad]*pad) 
+	else: 
+		return bytes([l]*l)
 
 
 if __name__ == '__main__':
