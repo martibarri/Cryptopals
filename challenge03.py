57f032942b0778031bcea7915ca8fad2f1f6384d
marti <martibarri23@gmail.com>
2018-06-17 20:28:51 +0200

    challenge03 improvement for challenge04

--- challenge03.py
+++ challenge03.py
@@ -1,12 +1,8 @@
 import operator
-from binascii import unhexlify
-
-# The hex encoded string has been XOR'd against a single character. Find the key, decrypt the message.
-input_string_hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
 
 def xor_cipher(string_hex):
-    string_dec = unhexlify(string_hex).decode('utf-8')
+    string_dec = (''.join([chr(int(''.join(c), 16)) for c in zip(string_hex[0::2], string_hex[1::2])]))
     xor_array = [""] * 256
     for i in range(0, 256):
         xor_tmp = ""
@@ -39,24 +35,33 @@ def xor_cipher_score(xor_array):
     return xor_dict
 
 
-def xor_result(xor_score_dict, cipher_array):
+def xor_result(xor_score_dict, cipher_array, string_hex):
     sorted_score = sorted(xor_score_dict.items(), key=operator.itemgetter(1),
                           reverse=True)  # [(88, 224), (95, 125), ... ] (key, score)
     result_array = [] * 255
     for j in range(0, 256):
-        result_array.append((sorted_score[j][0], sorted_score[j][1], cipher_array[sorted_score[j][0]]))
-        # [(key, score, string_decrypted), (), ()...] ordered by score
+        result_array.append((sorted_score[j][0], sorted_score[j][1], cipher_array[sorted_score[j][0]], string_hex))
+        # [(key, score, string_decrypted, input_string), (), ()...] ordered by score
     return result_array
 
 
-if __name__ == '__main__':
-    xor_array = xor_cipher(input_string_hex)  # cipher by every char
+def xor_decrypt(string_hex):
+    xor_array = xor_cipher(string_hex)  # cipher by every char
     xor_score = xor_cipher_score(xor_array)  # score
-    result = xor_result(xor_score, xor_array)
-    print('(key, score, string_decrypted)')
-    for j in range(0, 10):
-        print(result[j])
-    final_result = result[0]
+    result = xor_result(xor_score, xor_array, string_hex)  # order by score
+    # debug results:
+    # print('(key, score, string_decrypted)')
+    # for j in range(0, 10):
+    #   print(result[j])
+    final_result = result[0]  # first one with better score
+    return final_result
+
+
+if __name__ == '__main__':
+    # The hex encoded string has been XOR'd against a single character. Find the key, decrypt the message.
+    input_string_hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+    final_result = xor_decrypt(input_string_hex)
     print('final result:')
     print('key:', final_result[0], '(' + chr(final_result[0]) + ')')
     print('score:', final_result[1])
