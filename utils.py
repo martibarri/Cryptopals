67093abc175f3b22da378d1250ed8ef366d80d1d
marti <martibarri23@gmail.com>
2018-06-19 11:56:09 +0200

    challenge07 refactor

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
-from binascii import hexlify, unhexlify, b2a_base64
+from binascii import hexlify, unhexlify, b2a_base64, a2b_base64
 
 
 def hex2base64(input_hex):
     return b2a_base64(unhexlify(input_hex)).decode('utf-8')
+
+
+def base642hex(input_base64):
+    return hexlify(a2b_base64(input_base64))
 
 
 def xor_strings(s1, s2):
@@ -15,3 +19,8 @@ def hex2dec(string_hex):
 
 def dec2hex(string_dec):
     return hexlify(string_dec.encode('utf-8')).decode('utf-8')
+
+
+def pad_pkcs(text, l):
+    pad = (l - (len(text) % l)) % l
+    return text + bytes([pad] * pad) if len(text) != 0 else bytes([l] * l)
