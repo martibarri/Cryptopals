4863bf1100e2741bba6609ff999ec45407b2ac0a
marti <martibarri23@gmail.com>
2018-06-19 12:10:36 +0200

    challenge08 refactor

---
+++ challenge08.py
@@ -0,0 +1,26 @@
+# Detect AES in ECB mode
+
+if __name__ == '__main__':
+
+    f = open('sources/8.txt', 'r')
+    encrypted_data_hex = []
+    for line in f:
+        encrypted_data_hex.append(line.strip('\n'))
+
+    scores = []
+
+    for i in range(len(encrypted_data_hex)):
+        blocks = [encrypted_data_hex[i][j:j + 16] for j in range(0, len(encrypted_data_hex[i]), 16)]
+        score = len(blocks) - len(set(blocks))
+        scores.append(score)
+
+    max_score = max(scores)
+    positions = [i for i, j in enumerate(scores) if j == max_score]
+    # note that in fact only one score != 0
+    for i in range(len(positions)):
+        print("Ciphertext encrypted with AES-ECB:")
+        print([encrypted_data_hex[positions[i]][j:j + 16] for j in range(0, len(encrypted_data_hex[positions[i]]), 16)])
+        print("Location:", positions[i])
+        print("Number of repeated blocks:", scores[positions[i]])
+
+        #  the same 16 byte plaintext block will always produce the same 16 byte ciphertext
