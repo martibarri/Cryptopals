25a2f907c46f768815964e1c8eab551ab2504d86
marti <martibarri23@gmail.com>
2018-06-21 19:23:58 +0200

    cleaning

--- Set1/first_steps_python2/pal07.py
+++
@@ -1,9 +0,0 @@
-import base64
-from Crypto.Cipher import AES
-
-f = base64.b64decode(open('7.txt', 'r').read())
-
-key = b"YELLOW SUBMARINE"
-cipher = AES.new(key, AES.MODE_ECB)
-decrypted = cipher.decrypt(f)
-print(decrypted)
