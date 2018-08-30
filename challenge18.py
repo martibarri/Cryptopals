2975dfe7ee649ac921528fc784c787b9662b7c4a
marti <martibarri23@gmail.com>
2018-08-30 18:18:51 +0200

    challenge18 simplified

--- challenge18.py
+++ challenge18.py
@@ -3,28 +3,16 @@ from binascii import a2b_base64
 from utils import validate_pad_pkcs, unpad_pkcs
 
 
-def aes128_ctr_decrypt(cipher_text, nonce, key):
-    plain_text = b''
-    cipher_text_blocks = [cipher_text[i:i + 16] for i in range(0, len(cipher_text), 16)]
+def aes128_ctr_cipher(string, nonce, key):
+    cipher_string = b''
+    cipher_text_blocks = [string[i:i + 16] for i in range(0, len(string), 16)]
     for i in range(len(cipher_text_blocks)):
         nonce_block = nonce + i.to_bytes(8, byteorder='little')
         nonce_matrix = string_to_matrix_states(nonce_block)[0]
         nonce_matrix_cipher = aes128_RoundBlock(nonce_matrix, key)
         d = XorStates(nonce_matrix_cipher, string_to_matrix_states(cipher_text_blocks[i])[0])
-        plain_text += matrix_to_bytes(d)
-    return plain_text
-
-
-def aes128_ctr_encrypt(plain_text, nonce, key):
-    cipher_text = b''
-    plain_text_blocks = [plain_text[i:i + 16] for i in range(0, len(plain_text), 16)]
-    for i in range(len(plain_text_blocks)):
-        nonce_block = nonce + i.to_bytes(8, byteorder='little')
-        nonce_matrix = string_to_matrix_states(nonce_block)[0]
-        nonce_matrix_cipher = aes128_RoundBlock(nonce_matrix, key)
-        d = XorStates(nonce_matrix_cipher, string_to_matrix_states(plain_text_blocks[i])[0])
-        cipher_text += matrix_to_bytes(d)
-    return cipher_text
+        cipher_string += matrix_to_bytes(d)
+    return cipher_string
 
 
 if __name__ == '__main__':
@@ -38,8 +26,8 @@ if __name__ == '__main__':
     #  format = 64 bit unsigned little endian nonce,
     #           64 bit little endian block count (byte count / 16)
 
-    plain_text = aes128_ctr_decrypt(encrypted_data, nonce, key)
-    cipher_text = aes128_ctr_encrypt(plain_text, nonce, key)
+    plain_text = aes128_ctr_cipher(encrypted_data, nonce, key)
+    cipher_text = aes128_ctr_cipher(plain_text, nonce, key)
 
     if validate_pad_pkcs(cipher_text):
         print("cipher_text padded, unpadding...")
